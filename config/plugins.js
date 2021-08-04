76dd1a7761f6a1a0be4bf85904578c089f54a8a3
medquota <87965303+medquota@users.noreply.github.com>
2021-08-04 12:24:15 +0100

    Update plugins.js

--- config/plugins.js
+++ config/plugins.js
@@ -1,22 +1,34 @@
-module.exports = ({ env }) => {
+module.exports = {
+    //
+    graphql: {
+      endpoint: '/graphql',
+      tracing: true,
+      shadowCRUD: true,
+      playgroundAlways: false,
+      depthLimit: 7,
+      amountLimit: 50,
+    },
+  };
 
-    if(env('NODE_ENV') === 'production'){
-        return {
-            upload: {
-                provider: 'aws-s3',
-                providerOptions: {
-                    accessKeyId: env('AWS_ACCESS_KEY_ID'),
-                    secretAccessKey: env('AWS_ACCESS_SECRET'),
-                    region: env('AWS_REGION'),
-                    params: {
-                        Bucket: env('AWS_BUCKET'),
-                    },
-                },
-            },
-        }
-    }
+// module.exports = ({ env }) => {
 
-    return {
-        //Empty config which will use defaults
-    }
-}
+//     if(env('NODE_ENV') === 'production'){
+//         return {
+//             upload: {
+//                 provider: 'aws-s3',
+//                 providerOptions: {
+//                     accessKeyId: env('AWS_ACCESS_KEY_ID'),
+//                     secretAccessKey: env('AWS_ACCESS_SECRET'),
+//                     region: env('AWS_REGION'),
+//                     params: {
+//                         Bucket: env('AWS_BUCKET'),
+//                     },
+//                 },
+//             },
+//         }
+//     }
+
+//     return {
+//         //Empty config which will use defaults
+//     }
+// }
